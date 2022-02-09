3f2c1939f66195b6cec89a82e0be29912fa960a6
qizhen <lim.qizhen@gmail.com>
2022-02-09 13:09:33 +0800

    paper review done

--- src/components/PaperReview.js
+++ src/components/PaperReview.js
@@ -1,27 +1,63 @@
 //must retrieve the accuracy of the questions to display the right or wrong
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StudentNavBar from "./StudentNavBar";
 import { Grid, Button } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import DoneIcon from "@mui/icons-material/Done";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const PaperReview = () => {
   const params = useParams();
+  const user = useSelector((state) => state.user);
   console.log(params); //params.paper = paper_id
+  //retrieve the score
+  const [result,setResult] = useState()
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:8000/students/paper/score/${user.username}/${params.paper}/`
+      )
+      .then((res) => {
+        console.log(res.data[0].results)
+        setResult(res.data[0].results)
+      });
+  });
+  const resultDisplay = (Math.round(result*10000)/100).toFixed(2)
+  console.log(resultDisplay)
   //retrieve the questions for review
   const [questions, setQuestions] = useState([]);
   useEffect(() => {
     axios
       .get(
-        `http://localhost:8000/students/review_papers/${user.username}/${params.paper}`
+        `http://localhost:8000/students/review_papers/${user.username}/${params.paper}/`
       )
       .then((res) => {
         setQuestions(res.data);
       });
   }, []);
-  console.log(questions);
+  console.log(questions[0]);
+  const displayQuestions = questions.map((question, index) => {
+    return (
+      <li style={{ marginBottom: "10px" }}>
+        <img src={question.question_img} alt={`question ${index + 1}`} />
+        <br />
+        Your answer:{" "}
+        <span style={{ textDecoration: "underline" }}>
+          {question.student_answer}
+        </span>{" "}
+        Correct answer:{" "}
+        <span style={{ textDecoration: "underline" }}>{question.answer}</span>{" "}
+        {question.accuracy ? (
+          <DoneIcon style={{ color: "green" }} />
+        ) : (
+          <CloseIcon style={{ color: "red" }} />
+        )}
+      </li>
+    );
+  });
   return (
     <>
       <StudentNavBar />
@@ -33,33 +69,12 @@ const PaperReview = () => {
           display: "sticky",
         }}
       >
-        2017 O Levels Science (Physics)
-        <p style={{ fontWeight: "normal" }}>Score: 75%</p>
+        {params.paper.split("_")[0]} {params.paper.split("_")[1]} {params.paper.split("_")[2]}
+        <p style={{ fontWeight: "normal" }}>Score: {resultDisplay}%</p>
       </div>
       <div className="questions">
         <ol>
-          <li style={{ marginBottom: "10px" }}>
-            QUESTION
-            <img src={"https://imgur.com/YIybWQ0"} alt="question 1" />
-            <br />
-            Your answer: <span style={{ textDecoration: "underline" }}>
-              C
-            </span>{" "}
-            Correct answer:{" "}
-            <span style={{ textDecoration: "underline" }}>A</span>{" "}
-            <CloseIcon style={{ color: "red" }} />
-          </li>
-          <li style={{ marginBottom: "10px" }}>
-            QUESTION
-            <img src={"https://imgur.com/YIybWQ0"} alt="question 1" />
-            <br />
-            Your answer: <span style={{ textDecoration: "underline" }}>
-              C
-            </span>{" "}
-            Correct answer:{" "}
-            <span style={{ textDecoration: "underline" }}>C</span>{" "}
-            <DoneIcon style={{ color: "green" }} />
-          </li>
+          {displayQuestions}
         </ol>
       </div>
       <Grid container justifyContent="center">
